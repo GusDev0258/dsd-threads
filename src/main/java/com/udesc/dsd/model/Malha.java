6821e2636fa6b3cdb83c97e31b9dd4d0cfa141bc
gusdev0258 <16326176760@edu.udesc.br>
2024-04-30 19:48:42 -0300

    initiate project

---
+++ src/main/java/com/udesc/dsd/model/Malha.java
@@ -0,0 +1,17 @@
+package com.udesc.dsd.model;
+
+public class Malha {
+    public static final int NADA = 0;
+    public static final int ESTRADA_CIMA = 1;
+    public static final int ESTRADA_DIREITA = 2;
+    public static final int ESTRADA_BAIXO = 3;
+    public static final int ESTRADA_ESQUERDA = 4;
+    public static final int CRUZAMENTO_CIMA = 5;
+    public static final int CRUZAMENTO_DIREITA = 6;
+    public static final int CRUZAMENTO_BAIXO = 7;
+    public static final int CRUZAMENTO_ESQUERDA = 8;
+    public static final int CRUZAMENTO_CIMA_DIREITA = 9;
+    public static final int CRUZAMENTO_CIMA_ESQUERDA = 10;
+    public static final int CRUZAMENTO_BAIXO_DIREITA = 11;
+    public static final int CRUZAMENTO_BAIXO_ESQUERDA = 12;
+}
