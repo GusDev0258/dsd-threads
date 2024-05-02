d1004bd8dd61658ae2fac809c23bbd6371369920
tobiasfkk <78160146+tobiasfkk@users.noreply.github.com>
2024-05-01 22:39:12 -0300

    home layout

--- src/main/java/com/udesc/dsd/view/HomePageView.java
+++ src/main/java/com/udesc/dsd/view/HomePageView.java
@@ -1,25 +1,21 @@
 package com.udesc.dsd.view;
 
 import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 public class HomePageView {
     private JPanel panel1;
+    private JButton buttonSelecionarMalha;
+    private JButton buttonIniciar;
+    private JFormattedTextField numeroCarros;
+    private JRadioButton radioButtonSemaforo;
+    private JRadioButton radioButtonMonitor;
     private JButton testeButton;
 
     public HomePageView() {
         JFrame frame = new JFrame("dsd-threads");
-        frame.setSize(400, 300);
+        frame.setSize(250, 250);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.add(panel1);
         frame.setVisible(true);
-
-        testeButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                System.out.println("Botão pressionado!");
-            }
-        });
     }
 }
